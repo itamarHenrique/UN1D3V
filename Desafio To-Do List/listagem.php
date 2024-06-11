b57a21f4becc4904f53471bf4d3a796900da496c
itamarHenrique <itamarhjp1@gmail.com>
2024-06-10 22:15:28 -0300

    Adicionando pagina da listagem de tarefas

--- Desafio To-Do List/listagem.php
+++ Desafio To-Do List/listagem.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio To-Do List</title>
+    <title>Listagem</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./styles/style.css">
+
 </head>
 <body>
 
@@ -13,36 +14,18 @@
 
 <a class="navbar-brand">ToDo list</a>
 
-
 <ul class="nav justify-content-end">
-<li class="nav-item1 opacity-100">
+    <li class="nav-item1 opacity-100">
     <a class="nav-link" href="index.php">Cadastrar tarefas</a>
-</li>
-<li class="nav-item opacity-100">
+    </li>
+    <li class="nav-item opacity-100">
     <a class="nav-link" href="listagem.php">Listar tarefas</a>
-</li>
+    </li>
 </ul>
 
 </nav>
 
 
-<div class="container mt-5">
-    <h2>Nova Tarefa</h2>
-</div>
-
-
-<div class="container mt-5">
-    <div class="mb-3">
-    <label for="titulo" class="form-label">Titulo da tarefa</label>
-    <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Ex: Comprar leite">
-    </div>
-    <div class="mb-3">
-    <label for="data-tarefa" class="form-label">Data da tarefa</label>
-    <input type="text" class="form-control" id="data-tarefa" name="data-tarefa">
-    </div>
-    <button type="button" class="btn btn-primary">Cadastrar tarefa</button>
-
-</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
