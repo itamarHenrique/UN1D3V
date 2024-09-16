463912c8285066b863911f1361453d6f10e88c2e
itamarHenrique <itamarhjp1@gmail.com>
2024-09-16 03:43:21 -0300

    alteração no card

--- VueJS/rickNmorty/src/pages/index.vue
+++ VueJS/rickNmorty/src/pages/index.vue
@@ -1,109 +1,107 @@
 <template>
+  <v-card-text>
+    <v-select
+    label="Lista de Episódios"
+    :items="episodios.map(epi => epi.name)"
+    v-model="episodioSelecionado"
+    @change="carregaDetalhesEpisodio"
+    variant="solo-filled"
+    >
 
-  <v-combobox class="mx-auto my-8"
-  label="Lista de Episódios"
-  :items="episodios.map(ep => ep.name)"
-  v-model="episodioSelecionado"
-  @change="carregaDetalhesEpisodio"
-  ></v-combobox>
+    </v-select>
+  </v-card-text>
 
-
-<v-card
-    class="mx-auto my-8"
-    elevation="16"
+  <v-card
+    class="mx-auto"
     max-width="344"
+    title="Personagens: "
   >
-    <v-card-item>
-      <v-card-title>
-        Personagens:
-      </v-card-title>
-    </v-card-item>
 
-    <v-img
-      v-if="detalheEpisodios"
-      :src="detalheEpisodios.image"
-      aspect-ratio="1"
-    ></v-img>
+    <v-list>
+      <v-list-item
+        v-for="personagem in personagens"
+        :key="personagem.id"
+      >
+        <v-avatar>
+          <v-img :src="personagem.image" />
+        </v-avatar>
 
-
-      <v-list>
-        <v-list-item
-          v-for="personagem in personagens"
-          :key="personagem.id"
-        >
-          <v-avatar>
-            <v-img :src="personagem.image" />
-          </v-avatar>
-
-
-            <v-list-item-title>{{ personagem.name }}</v-list-item-title>
-            <v-list-item-subtitle>Last known location: {{ personagem.location.name }}</v-list-item-subtitle>
-            <v-list-item-subtitle>First seen in: {{ personagem.episode[0] }}</v-list-item-subtitle>
-
-        </v-list-item>
+        <v-list-item-title>{{ personagem.name }}</v-list-item-title>
+        <v-list-item-subtitle>Last known location: {{ personagem.location.name }}</v-list-item-subtitle>
+        <v-list-item-subtitle>First seen in: {{ personagem.episode[0] }}</v-list-item-subtitle>
+      </v-list-item>
     </v-list>
   </v-card>
-
-
 </template>
 
 <script>
-
 import axios from 'axios';
 
-  export default {
-    data() {
-      return {
-        episodios: [],
-        personagens: [],
-        episodioSelecionado: null,
-        carregaDetalhes: null,
-        detalheEpisodios: null
-      }
-    },
-    mounted() {
-      this.carregarEpisodios()
-    },
-    methods: {
-      carregarEpisodios(){
-          const url = 'https://rickandmortyapi.com/api/episode'
-
-        axios.get(url)
-        .then( response => {
-          this.episodios = response.data.results;
-        }).catch( error => {
-          console.log(error)
-        })
-
-      },
-
-      carregaDetalhesEpisodio() {
-      const episodio = this.episodios.find(ep => ep.name === this.episodioSelecionado);
-
-      if (episodio) {
-        const url = `https://rickandmortyapi.com/api/episode/${episodio.id}`;
-
-        axios.get(url)
-          .then(response => {
-            this.carregaDetalhes = response.data;
-            this.carregaDetalhesPersonagens(response.data.characters);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
-    },
-    carregaDetalhesPersonagens(personagensUrls) {
-      const requisicoes = personagensUrls.map(url => axios.get(url));
-
-      Promise.all(requisicoes)
+export default {
+  data() {
+    return {
+      episodios: [],
+      personagens: [],
+      episodioSelecionado: null,
+      detalheDoEpisodio: null
+    };
+  },
+  mounted() {
+    this.carregarEpisodios();
+  },
+  methods: {
+    carregarEpisodios() {
+      const url = 'https://rickandmortyapi.com/api/episode';
+      axios
+        .get(url)
         .then(response => {
-          this.personagens = response.map(res => res.data);
+          this.episodios = response.data.results;
+          console.log('Episódios carregados:', this.episodios);
         })
         .catch(error => {
           console.log(error);
         });
-    }
-  }
+},
+    carregaDetalhesEpisodio() {
+      const episodio = this.episodios.find(ep => ep.name === this.episodioSelecionado);
+
+      console.log('Episódio selecionado:', this.episodioSelecionado);
+      console.log('Episódio encontrado:', episodio);
+
+      if (episodio && episodio.id) {
+        const url = `https://rickandmortyapi.com/api/episode/${episodio.id}`;
+        console.log('URL de requisição:', url);
+
+        axios
+          .get(url)
+          .then(response => {
+            this.detalheDoEpisodio = response.data;
+            console.log('Detalhes do episódio:', this.detalheDoEpisodio);
+            this.carregaPersonagens(response.data.characters);
+          })
+          .catch(error => {
+            console.log('Erro na requisição do episódio:', error);
+          });
+      } else {
+        console.log('Episódio não encontrado ou sem ID.');
+      }
+},
+      carregaPersonagens(urls) {
+        console.log('URLs recebidas para personagens:', urls);
+
+        const requisicoes = urls.map(url => axios.get(url));
+
+        Promise.all(requisicoes)
+          .then(responses => {
+            console.log('Respostas das requisições:', responses);
+            this.personagens = responses.map(response => response.data);
+            console.log('Personagens carregados:', this.personagens);
+          })
+          .catch(error => {
+            console.log('Erro ao carregar personagens:', error);
+          });
 }
+
+  }
+};
 </script>
