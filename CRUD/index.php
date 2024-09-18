99630e995157064cb4753e968738ac000bace2b9
itamarHenrique <itamarhjp1@gmail.com>
2024-09-18 12:53:28 -0300

    Alteração na cor do fundo e centralizando o index

--- CRUD/index.php
+++ CRUD/index.php
@@ -16,11 +16,10 @@ $result = $mysqli->query('SELECT * FROM crud');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud PHP</title>
-    <link rel="stylesheet" href="./styles/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
-<body style="background-color: #F0F0F0">
+<body style="background-color: #F8F9FA">
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" >Cadastro de Usuarios</a>
@@ -30,35 +29,50 @@ $result = $mysqli->query('SELECT * FROM crud');
   </div>
 </nav>
 
+<div class="container d-flex flex-column justify-content-center align-items-center mt-5">
 
-<h2>Lista de Úsuarios</h2>
+<h2 class="text-center mb-4">Lista de Úsuarios</h2>
 
-<table border="1">
+<div class="table-responsive w-75">
+    <table class="table table-bordered text-center">
 
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Email</th>
-        <th>Ações</th>
-    </tr>
-    
+    <thead>
+
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
     <?php while($row = $result->fetch_assoc()): ?>
         <tr>
-            <th><?php echo $row['id']; ?></th>
-            <th><?php echo $row['nome']; ?></th>
-            <th><?php echo $row['email'] ;?></th>
-            <th><a href="editar.php?id=<?php echo $row['id']; ?>">Editar</a>
-            <a onclick="return confirmarExclusao()" href="delete.php?id=<?php echo $row['id']; ?>">Deletar</a>
-            </th>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['nome']; ?></td>
+            <td><?php echo $row['email'] ;?></td>
+            <td><a href="editar.php?id=<?php echo $row['id']; ?>"><button class="btn btn-light">Editar</button></a>
+            <a onclick="return confirmarExclusao()" href="delete.php?id=<?php echo $row['id']; ?>"><button class="btn btn-danger">Deletar</button></a>
+            </td>
         </tr>
 
-        <?php endwhile; ?>
+    <?php endwhile; ?>
 
-</table>
 
-<div class="botao">
+    </tbody>
+
+    </table>
+</div>
+
+<div class="text-center mt-3">
     <a href="criar.php"><button style="width: 200px; height: 35px">Adicionar Novo Úsuario</button></a>
 </div>
+
+</div>
+
+
+</div>
+
 
 <script>
 
