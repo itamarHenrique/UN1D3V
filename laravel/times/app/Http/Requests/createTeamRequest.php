eb2c1e75dd7621b5bcd764fb98d664b8662a1074
itamarHenrique <itamarhjp1@gmail.com>
2024-10-02 01:12:21 -0300

    Adição da validação de criar time

---
+++ laravel/times/app/Http/Requests/createTeamRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class createTeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:30', 'min:3'],
+            'foundation_date' => ['required'],
+            'id' => ['required'],
+            'players' => ['array', 'max:3'],
+            'players.*.name' => 'required|min:3|max:30',
+            'players.*.position' => 'required'
+        ];
+    }
+}
