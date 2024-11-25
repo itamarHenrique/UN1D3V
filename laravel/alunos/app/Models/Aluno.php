dad9b4fa124dd7d430ed31cce91bbb71a02c55a5
itamarHenrique <itamarhjp1@gmail.com>
2024-11-25 10:01:01 -0300

    Adicionando função getAll e getById

---
+++ laravel/alunos/app/Models/Aluno.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aluno extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = ['nome_aluno', 'RA','email','unidade_de_ensino'];
+
+    protected $aluno = 'alunos';
+
+    public function getAll()
+    {
+        return Aluno::all();
+    }
+
+    public function getById($id)
+    {
+        return Aluno::findOrFail($id);
+    }
+}
